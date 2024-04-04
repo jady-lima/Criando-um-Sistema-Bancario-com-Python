6ecc2e234a1356b6700c05c185a10416a555725f
Jady Lima <jadylsilva@gmail.com>
2024-04-04 16:48:29 -0300

    Update desafio.py

--- desafio.py
+++ desafio.py
@@ -15,21 +15,41 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        print("d")
-        break
+        deposito = float(input("Valor do depósito: "))
+        
+        if deposito > 0:
+            saldo += deposito
+            extrato += f"Depósito: R$ {deposito:.2f}\n"
+        else:
+            print("Não é possível realizar a operação com saldo negativo. Por favor, recomece a operação.")
 
     elif opcao == "s":
-        print("s")
-        break
+        saque = float(input("Valor do saque: "))
+        
+        if numero_saques < LIMITE_SAQUES:
+            if saque <= 500:
+                if saque <= saldo and saque > 0:
+                    saldo -= saque
+                    extrato += f"Saque: R$ {saque:.2f}\n"
+                    numero_saques += 1
+                    print("Saque efetuado com sucesso!")
+                else:
+                    print("Não possivel efetuar o saque por falta de limite.")
+            else:
+                print("Saque maior que o limite permitido.")
+        else:
+            print("Limite de saque diário atingido")
 
     elif opcao == "e":
-        print("e")
-        break
+        if extrato == "":
+            print("Não foram realizadas movimentações.")
+        else:
+            print("-" * 25)
+            print(f"{extrato}Saldo final: R$ {saldo}")
+            print("-" * 25)
 
     elif opcao == "q":
-        print("q")
         break
 
     else:
-        print("Nenhuma opção selecionada")
-        break
+        print("Nenhuma opção selecionada. Por favor, selecione uma opção seguindo o menu.")
